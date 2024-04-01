863fd2e7a5739822ddbbb497eda4bce51070b951
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2024-04-01 13:49:02 +0530

    order columns

--- pages/portfolio_level.py
+++ pages/portfolio_level.py
@@ -16,10 +16,11 @@ color_palette = ["#e07a5f", "#3d405b", "#81b29a", "#f2cc8f", "#f4f1de"]
 st.subheader("Portfolio level Average Damage Ratio")
 data4 = format_transform_bar(df, 70, 75)
 data4['key'] = data4['key'].apply(format_scenario)
-bar(data4, 'value:Q', 'key:O', '', 'Average Damage Ratio')
+bar(data4, 'value:Q', 'key:O', '',
+    'Average Damage Ratio', order=["Scenario Baseline"])
 
 st.subheader("Portfolio Level Total Loss in Miillion US Dollars")
-data5 = format_transform_bar(df, 66, 70)
+data5 = format_transform_bar(df, 66, 71)
 data5['key'] = data5['key'].apply(format_scenario)
 data5['value'] = data5['value']/1000
 bar(data5, 'value:Q', 'key:O', '', 'Total Loss in Miillion US $')
@@ -27,4 +28,5 @@ bar(data5, 'value:Q', 'key:O', '', 'Total Loss in Miillion US $')
 st.subheader("Portfolio Level- Mean Damage Ratio Index")
 data6 = format_transform_bar(df, 75)
 data6['key'] = data6['key'].apply(format_scenario)
-bar(data6, 'value:Q', 'key:O', '', 'Mean Damage Ratio Index')
+bar(data6, 'value:Q', 'key:O', '',
+    'Mean Damage Ratio Index', order=["Scenario Baseline"])
